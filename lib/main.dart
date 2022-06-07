7948d32cc09489b75d8080d3131ba7b78b7e27be
yosiruma <yosiruma@gmail.com>
2022-06-07 22:55:34 +0900

    オーバーフロー時のスクロールに対応

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
             Image.asset(
               'images/lake.jpg',
